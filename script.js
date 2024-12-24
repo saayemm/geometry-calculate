23b4031b6bae712d2291e0134b8ded68aac45010
Saayemm <sayemdcc@gmail.com>
2024-12-24 14:01:16 +0600

    final

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-const year = new Date().getFullYear();
-document.getElementById("year").textContent = year;
 
 function calculateTriangle() {
     const base  = getInputValue('base');
     const height = getInputValue('height');
     const area = 0.5 * base * height;
-    console.log(area);
 
     setInnerValue('show-result', area);
+}
+
+function calculateRectangular(){
+    const baseRec = getInputValue('rec');
+    const heightRec = getInputValue('height-rec');
+    const areaRec = baseRec * heightRec;
+
+    setInnerValue('show-result-rec', areaRec);
+}
+
+function calculateParallelogram(){
+    const basePar = getInputValue('base-p');
+    const heightPar = getInputValue('height-p');
+    const areaPar = basePar * heightPar;
+
+    setInnerValue('show-result-p', areaPar);
+}
+
+function calculateRhombus(){
+    const d1 = getInputValue('base-rhom');
+    const d2 = getInputValue('height-rhom');
+    const areaRh = 0.5 * d1 * d2;
+
+    setInnerValue('show-result-rhym', areaRh);
+}
+
+function calculatePentagon(){
+    const p = getInputValue('base-penta');
+    const a = getInputValue('height-penta');
+    const areaPenta = 0.5 * p * a;
+
+    setInnerValue('show-result-penta', areaPenta);
+}
+
+function calculateEllipse(){
+    const a = getInputValue('base-elip');
+    const b = getInputValue('height-elip');
+    const areaEll = (Math.PI * a * b).toFixed(2);
+
+    setInnerValue('show-result-elip', areaEll);
 }
 
 function getInputValue(inputId){
@@ -22,3 +59,7 @@ function setInnerValue(elementId, area){
    const elem = document.getElementById(elementId);
    elem.innerText = area;
 }
+
+
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
